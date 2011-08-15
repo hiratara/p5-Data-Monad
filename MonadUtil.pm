4630e0bdca52baf55c3051ce04aad913f8279832
Masahiro Honma <hiratara@cpan.org>
2011-08-15 17:45:11 +0900

    Split into two files.

--- MonadUtil.pm
+++ MonadUtil.pm
@@ -1,8 +1,10 @@
+package MonadUtil;
+use AnyEvent;
 use strict;
 use warnings;
-use AnyEvent;
-use AnyEvent::HTTP;
-use AnyEvent::Util;
+use Exporter 'import';
+
+our @EXPORT = qw/m_unit m_join m_map m_bind/;
 
 sub m_unit($) {
 	my $v = shift;
@@ -47,28 +49,4 @@ sub m_bind($$) {
 	return m_join $cv2;
 }
 
-sub get_url($) {
-	my $url = shift;
-	my $ret_cv = AE::cv;
-	http_get $url, sub {
-		$ret_cv->send($_[0]);
-	};
-	return $ret_cv;
-}
-
-sub count_line($) {
-	my $html = shift;
-
-	my $cv = AnyEvent::Util::run_cmd(
-		[qw/wc /], '<' => \$html, '>' => \my $ret,
-	);
-	return m_bind $cv => sub {m_unit $ret};
-}
-
-my $ret_cv = m_bind get_url("http://www.google.com")
-                    => \&count_line;
-$ret_cv->cb(sub {
-	print $_[0]->recv, "\n";
-});
-
-$ret_cv->recv;
+1;
