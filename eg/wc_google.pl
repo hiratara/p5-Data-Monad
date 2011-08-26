19dbd8270452a04c20ef3fd7bccf25c54a8eff83
Masahiro Honma <hiratara@cpan.org>
2011-08-26 13:54:47 +0900

    Rewrite sample by using m_binds()

--- eg/wc_google.pl
+++ eg/wc_google.pl
@@ -4,28 +4,18 @@ use MonadUtil;
 use AnyEvent::HTTP;
 use AnyEvent::Util;
 
-sub get_url($) {
+print m_binds(m_unit("http://www.google.com") => sub {
 	my $url = shift;
 	my $ret_cv = AE::cv;
 	http_get $url, sub {
 		$ret_cv->send($_[0]);
 	};
 	return $ret_cv;
-}
-
-sub count_line($) {
+}, sub {
 	my $html = shift;
 
-	my $cv = AnyEvent::Util::run_cmd(
-		[qw/wc /], '<' => \$html, '>' => \my $ret,
-	);
-	return m_bind $cv => sub {m_unit $ret};
-}
-
-my $ret_cv = m_bind get_url("http://www.google.com")
-                    => \&count_line;
-$ret_cv->cb(sub {
-	print $_[0]->recv, "\n";
-});
-
-$ret_cv->recv;
+	my $ret;
+	m_bind AnyEvent::Util::run_cmd(
+		[qw/wc /], '<' => \$html, '>' => \$ret,
+	) => sub {m_unit $ret};
+})->recv, "\n";
