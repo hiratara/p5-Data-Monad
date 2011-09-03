a8f3f9d154ee5b1f8a240dbbf17c77f13e0555d5
Masahiro Honma <hiratara@cpan.org>
2011-09-03 22:41:44 +0900

    Run module-setup.

--- lib/Data/Monad.pm
+++ lib/Data/Monad.pm
@@ -51,3 +51,31 @@ sub flatten {
 sub ap { (ref $_[0])->lift(sub { my $c = shift; $c->(@_) })->(@_) }
 
 1;
+
+__END__
+
+=head1 NAME
+
+Data::Monad -
+
+=head1 SYNOPSIS
+
+  use Data::Monad;
+
+=head1 DESCRIPTION
+
+Data::Monad is
+
+=head1 AUTHOR
+
+hiratara E<lt>hiratara {at} cpan.orgE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
+
