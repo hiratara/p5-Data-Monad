215b8111695a503468523bb18a6e4405f6974860
Masahiro Honma <hiratara@cpan.org>
2011-09-06 09:59:01 +0900

    Stop the overriding which improves the performance.
    This reverts commit dc3130e7c5315aee8e9e51650773d2276c5b907b,
                    and c6f5f3a4c4c35ce96ac07a7e695f3349f86cc2b9.
    
    Invocation of cv's method is the overhead right there.
    But it's not serious when you write asynchronous codes.
    If it becomes problem later, just revert this commit.

--- eg/bench_cv.pl
+++ eg/bench_cv.pl
@@ -85,27 +85,29 @@ Benchmark::cmpthese($r);
 __END__
 
 % perl -Ilib eg/bench_cv.pl .0001 10000
-Benchmark: timing 10000 iterations of bare_ae, coro, monad, monad_for...
-   bare_ae: 1.14658 wallclock secs ( 0.71 usr +  0.06 sys =  0.77 CPU) @ 12987.01/s (n=10000)
-      coro: 1.1488 wallclock secs ( 0.71 usr +  0.06 sys =  0.77 CPU) @ 12987.01/s (n=10000)
-     monad: 1.07258 wallclock secs ( 0.83 usr +  0.05 sys =  0.88 CPU) @ 11363.64/s (n=10000)
- monad_for: 1.21419 wallclock secs ( 1.18 usr +  0.03 sys =  1.21 CPU) @ 8264.46/s (n=10000)
-            Rate monad_for      coro   bare_ae     monad
-monad_for 4118/s        --       -5%       -6%      -12%
-coro      4352/s        6%        --       -0%       -7%
-bare_ae   4361/s        6%        0%        --       -6%
-monad     4662/s       13%        7%        7%        --
+Benchmark: timing 10000 iterations of bare_ae, coro, monad, monad_for, monad_lift...
+   bare_ae: 1.16068 wallclock secs ( 0.72 usr +  0.06 sys =  0.78 CPU) @ 12820.51/s (n=10000)
+      coro: 1.16513 wallclock secs ( 0.72 usr +  0.06 sys =  0.78 CPU) @ 12820.51/s (n=10000)
+     monad: 1.10253 wallclock secs ( 0.87 usr +  0.05 sys =  0.92 CPU) @ 10869.57/s (n=10000)
+ monad_for: 1.21874 wallclock secs ( 1.18 usr +  0.03 sys =  1.21 CPU) @ 8264.46/s (n=10000)
+monad_lift: 1.33561 wallclock secs ( 1.30 usr +  0.03 sys =  1.33 CPU) @ 7518.80/s (n=10000)
+             Rate monad_lift  monad_for       coro    bare_ae      monad
+monad_lift 3744/s         --        -9%       -13%       -13%       -17%
+monad_for  4103/s        10%         --        -4%        -5%       -10%
+coro       4291/s        15%         5%         --        -0%        -5%
+bare_ae    4308/s        15%         5%         0%         --        -5%
+monad      4535/s        21%        11%         6%         5%         --
 
 % perl -Ilib eg/bench_cv.pl 0 20000
 Benchmark: timing 20000 iterations of bare_ae, coro, monad, monad_for, monad_lift...
-   bare_ae: 0.980902 wallclock secs ( 0.94 usr +  0.04 sys =  0.98 CPU) @ 20408.16/s (n=20000)
-      coro: 0.970281 wallclock secs ( 0.93 usr +  0.04 sys =  0.97 CPU) @ 20618.56/s (n=20000)
-     monad: 1.33999 wallclock secs ( 1.30 usr +  0.05 sys =  1.35 CPU) @ 14814.81/s (n=20000)
- monad_for: 2.14951 wallclock secs ( 2.09 usr +  0.06 sys =  2.15 CPU) @ 9302.33/s (n=20000)
-monad_lift: 1.38826 wallclock secs ( 1.34 usr +  0.04 sys =  1.38 CPU) @ 14492.75/s (n=20000)
-              Rate  monad_for monad_lift      monad    bare_ae       coro
-monad_for   4652/s         --       -35%       -38%       -54%       -55%
-monad_lift  7203/s        55%         --        -3%       -29%       -30%
-monad       7463/s        60%         4%         --       -27%       -28%
-bare_ae    10195/s       119%        42%        37%         --        -1%
-coro       10306/s       122%        43%        38%         1%         --
+   bare_ae: 0.991276 wallclock secs ( 0.94 usr +  0.04 sys =  0.98 CPU) @ 20408.16/s (n=20000)
+      coro: 1.01837 wallclock secs ( 0.97 usr +  0.05 sys =  1.02 CPU) @ 19607.84/s (n=20000)
+     monad: 1.7295 wallclock secs ( 1.68 usr +  0.04 sys =  1.72 CPU) @ 11627.91/s (n=20000)
+ monad_for: 2.51117 wallclock secs ( 2.44 usr +  0.06 sys =  2.50 CPU) @ 8000.00/s (n=20000)
+monad_lift: 2.69432 wallclock secs ( 2.63 usr +  0.07 sys =  2.70 CPU) @ 7407.41/s (n=20000)
+              Rate monad_lift  monad_for      monad       coro    bare_ae
+monad_lift  3712/s         --        -7%       -36%       -62%       -63%
+monad_for   3982/s         7%         --       -31%       -59%       -61%
+monad       5782/s        56%        45%         --       -41%       -43%
+coro        9820/s       165%       147%        70%         --        -3%
+bare_ae    10088/s       172%       153%        74%         3%         --
