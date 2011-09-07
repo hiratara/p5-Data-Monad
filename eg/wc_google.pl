4fdedb12a0c8a4976204ef1d1fa11cc9bc3fb9db
Masahiro Honma <hiratara@cpan.org>
2011-09-07 12:17:25 +0900

    Simpify sample.

--- eg/wc_google.pl
+++ eg/wc_google.pl
@@ -4,15 +4,12 @@ use Data::Monad::CondVar;
 use AnyEvent::HTTP;
 use AnyEvent::Util;
 
-print AnyEvent::CondVar->unit("http://www.google.com")->flat_map(sub {
-    my $url = shift;
-    http_get $url, (my $ret_cv = AE::cv);
-    $ret_cv;
-})->flat_map(sub {
+my $url = "http://www.google.com";
+print as_cv { http_get $url, $_[0] }->flat_map(sub {
     my $html = shift;
 
     my $ret;
     AnyEvent::Util::run_cmd(
         [qw/wc /], '<' => \$html, '>' => \$ret,
-    )->flat_map(sub { AnyEvent::CondVar->unit($ret) });
+    )->map(sub { $ret });
 })->recv, "\n";
